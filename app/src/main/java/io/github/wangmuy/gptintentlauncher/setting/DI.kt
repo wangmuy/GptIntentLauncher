7c11e4ab21f16dfa6461af4ac31ddf1b5fa1e40f
wangmuy <wangmuy@gmail.com>
2023-08-27 13:39:00 +0800

    settings save to shared preferences

--- app/src/main/java/io/github/wangmuy/gptintentlauncher/setting/DI.kt
+++ app/src/main/java/io/github/wangmuy/gptintentlauncher/setting/DI.kt
@@ -3,6 +3,6 @@ package io.github.wangmuy.gptintentlauncher.setting
 import org.koin.dsl.module
 
 val settingModule = module {
-    single<SettingDataSource> { SettingRepository() }
+    single<SettingDataSource> { SettingRepository(get()) }
     factory { SettingScreenViewModel(get(), get()) }
 }
--- app/src/main/java/io/github/wangmuy/gptintentlauncher/setting/SettingRepository.kt
+++ app/src/main/java/io/github/wangmuy/gptintentlauncher/setting/SettingRepository.kt
@@ -1,7 +1,8 @@
 package io.github.wangmuy.gptintentlauncher.setting
 
+import android.content.Context
+import android.content.SharedPreferences
 import com.wangmuy.llmchain.llm.BaseLLM
-import com.wangmuy.llmchain.serviceprovider.openai.OpenAIChat
 import io.github.wangmuy.gptintentlauncher.data.model.ChatConfig
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -10,9 +11,10 @@ import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.update
 
-class SettingRepository: SettingDataSource {
+class SettingRepository(appContext: Context): SettingDataSource {
     companion object {
         private const val TAG = "SettingRepository"
+        private const val SP_NAME = "settings"
 
         private const val KEY_APIKEY = "apiKey"
         private const val KEY_BASEURL = "baseUrl"
@@ -46,12 +48,23 @@ class SettingRepository: SettingDataSource {
         }
     }
 
+    private val sharedPref: SharedPreferences = appContext.getSharedPreferences(SP_NAME, Context.MODE_PRIVATE)
     private val _configMSF = MutableStateFlow(getPersistedConfig())
     private val config = _configMSF.asStateFlow()
     private val observableConfig: Flow<ChatConfig> = config.map { it }
 
     private fun getPersistedConfig(): ChatConfig {
         val cfg = ChatConfig()
+        val apiKey = sharedPref.getString(KEY_APIKEY, cfg.apiKey)!!
+        val baseUrl = sharedPref.getString(KEY_BASEURL, cfg.baseUrl)!!
+        val timeoutMillis = sharedPref.getLong(KEY_TIMEOUT_MILLIS, cfg.timeoutMillis)
+        val proxy = sharedPref.getString(KEY_PROXY, cfg.proxy)!!
+        val llmConfig = sharedPref.getString(KEY_LLMCONFIG, llmConfigToString(cfg.llmConfig))!!
+        cfg.apiKey = apiKey
+        cfg.baseUrl = baseUrl
+        cfg.timeoutMillis = timeoutMillis
+        cfg.proxy = proxy
+        cfg.llmConfig.putAll(llmConfigFromString(llmConfig))
         return cfg
     }
 
@@ -66,22 +79,33 @@ class SettingRepository: SettingDataSource {
     override suspend fun saveConfig(config: ChatConfig) {
         _configMSF.update {
             val newConfig = it.copy()
+            val edit = sharedPref.edit()
             if (config.apiKey.isNotEmpty()) {
                 val apiKey = config.apiKey
                 newConfig.apiKey = apiKey
+                edit.putString(KEY_APIKEY, apiKey)
             }
             if (config.baseUrl.isNotEmpty()) {
                 val baseUrl = config.baseUrl
                 newConfig.baseUrl = baseUrl
+                edit.putString(KEY_BASEURL, baseUrl)
+            }
+            if (config.timeoutMillis >= 0) {
+                val timeoutMillis = config.timeoutMillis
+                newConfig.timeoutMillis = timeoutMillis
+                edit.putLong(KEY_TIMEOUT_MILLIS, timeoutMillis)
             }
             if (config.proxy.isNotEmpty()) {
                 val proxy = config.proxy
                 newConfig.proxy = proxy
+                edit.putString(KEY_PROXY, proxy)
             }
             if (config.llmConfig.isNotEmpty()) {
                 val llmConfig = config.llmConfig
                 newConfig.llmConfig = llmConfig
+                edit.putString(KEY_LLMCONFIG, llmConfigToString(llmConfig))
             }
+            edit.apply()
             newConfig
         }
     }
